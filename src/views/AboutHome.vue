0890c7044565da68baec579b0c86157f9862293f
Hendrixonfire <129618415+Hendrixonfire@users.noreply.github.com>
2023-11-29 01:18:18 +0300

    Add files via upload

---
+++ src/views/AboutHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About us</h1>
+    <p>This is "About us" page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // Логика компонента About.vue
+};
+</script>
+
+<style scoped>
+/* Стили для About.vue */
+</style>
